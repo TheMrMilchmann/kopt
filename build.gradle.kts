8a705485661a11c1f934e2aa4130939bf8bb9785
Leon Linhart <themrmilchmann@gmail.com>
2018-01-02 12:19:33 +0100

    chore: bump version number {0.1.1 => 0.1.2}

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     kotlin("jvm") version "1.2.10"
 }
 
-val nextVersion = "0.1.1"
+val nextVersion = "0.1.2"
 
 group = "com.github.themrmilchmann.kopt"
 version = when (deployment.type) {
