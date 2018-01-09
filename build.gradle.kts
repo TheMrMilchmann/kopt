7629845ddab021c5c668c2b0520034596317c558
Leon Linhart <themrmilchmann@gmail.com>
2018-01-09 14:35:46 +0100

    chore: bump version number {0.2.0 => 0.2.1}
    Something caused a faulty kotlin_module file to be generated which made important API functions inaccessible.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     kotlin("jvm") version "1.2.10"
 }
 
-val nextVersion = "0.2.0"
+val nextVersion = "0.2.1"
 
 group = "com.github.themrmilchmann.kopt"
 version = when (deployment.type) {
